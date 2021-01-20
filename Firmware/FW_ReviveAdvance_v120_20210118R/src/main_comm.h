300f8d26f87016f0cac78e625e813b36a0a16093
takeokk <42992638+takeokk@users.noreply.github.com>
2021-01-20 14:31:12 +0900

    Ver120 publish

---
+++ Firmware/FW_ReviveAdvance_v120_20210118R/src/main_comm.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_COMM_H
+#define MAIN_COMM_H
+
+#include "app.h"
+#include "./USB/usb.h"
+#include "./USB/usb_function_hid.h"
+
+
+extern void USB_Comm(void);
+
+
+#endif //MAIN_COMM_H
+
