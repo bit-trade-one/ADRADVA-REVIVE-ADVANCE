2c9b00237fe3d76161efa96ec19f09c750b5e24a
kit-bto <i.kitahara@bit-trade-one.co.jp>
2021-01-20 15:01:42 +0900

    Update tool / Firmware

---
+++ Firmware/FW_ReviveAdvance_v120_20210118R/ReviveAdvance.X/build/default/production/_ext/1360937237/l_eeprom.o.d
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+build/default/production/_ext/1360937237/l_eeprom.o:  \
+../src/l_eeprom.c  \
+ ../src/app.h  \
+../src/Common/GenericTypeDefs.h  \
+../src/./USB/usb.h  \
+ ../src/Common/Compiler.h  \
+../src/usb_config.h  \
+../src/USB/usb_common.h  \
+ ../src/USB/usb_ch9.h  \
+../src/USB/usb_device.h  \
+../src/USB/usb_hal.h  \
+ ../src/USB/usb_hal_pic32.h  \
+../src/l_spi.h  \
+../src/l_eeprom.h  \
+ ../src/main_sub.h 
+=======
+build/default/production/_ext/1360937237/l_eeprom.o:  \
+../src/l_eeprom.c  \
+ ../src/app.h  \
+../src/Common/GenericTypeDefs.h  \
+../src/./USB/usb.h  \
+ ../src/Common/Compiler.h  \
+../src/usb_config.h  \
+../src/USB/usb_common.h  \
+ ../src/USB/usb_ch9.h  \
+../src/USB/usb_device.h  \
+../src/USB/usb_hal.h  \
+ ../src/USB/usb_hal_pic32.h  \
+../src/l_spi.h  \
+../src/l_eeprom.h  \
+ ../src/main_sub.h 
+>>>>>>> Upload-ready
---
+++ Firmware/FW_ReviveAdvance_v120_20210118R/src/main_comm.h
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+#ifndef MAIN_COMM_H
+#define MAIN_COMM_H
+
+#include "app.h"
+#include "./USB/usb.h"
+#include "./USB/usb_function_hid.h"
+
+
+extern void USB_Comm(void);
+
+
+#endif //MAIN_COMM_H
+
+=======
+#ifndef MAIN_COMM_H
+#define MAIN_COMM_H
+
+#include "app.h"
+#include "./USB/usb.h"
+#include "./USB/usb_function_hid.h"
+
+
+extern void USB_Comm(void);
+
+
+#endif //MAIN_COMM_H
+
+>>>>>>> Upload-ready
